3aad59fd9d06538cec524d61efe846f250ee7793
Derek Roman <derek.roman81@gmail.com>
2018-04-29 22:09:35 -0500

    PhoneScreen
    ASP MVC page for an interview questionaire

---
+++ PhoneInterviewScreenSystem/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PhoneInterviewScreenSystem.Models;
+
+namespace PhoneInterviewScreenSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Phone Screen App Usage.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Contact us for any feedback, problems, or enhancements that you may find. ";
+
+            return View();
+        }
+
+        public IActionResult PhoneScreen()
+        {
+            ViewData["Message"] = "Screen a new applicant";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PhoneInterviewScreenSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhoneInterviewScreenSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
